5eff06246c4246a79c165ad6f71664ea56e50ca8
Herbie Vine <vineherbie@gmail.com>
2021-05-22 20:36:51 +0100

    redeploy next env

--- utils/withApollo.ts
+++ utils/withApollo.ts
@@ -3,8 +3,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 const apolloClient = new ApolloClient({
   uri: process.env.CIRCLECI
-    ? process.env.NEXT_PUBLIC_TEST_API_URL
-    : process.env.NEXT_PUBLIC_API_URL,
+    ? process.env.NEXT_PUBLIC_API_URL
+    : process.env.NEXT_PUBLIC_TEST_API_URL,
   // credentials: 'include',
   cache: new InMemoryCache(),
 })
