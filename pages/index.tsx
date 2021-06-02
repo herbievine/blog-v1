005a16ee1a70c38d50a7b3240c6ad163c9bf3eff
Tim Vine <Home@tims-macbook-pro.home>
2021-06-02 16:36:18 +0200

    fixed index layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import { ArrowRightIcon } from '@heroicons/react/solid'
 import React from 'react'
-import IndexLayout from '../components/layout/IndexLayout'
 import Navigation from '../components/modules/Navigation'
+import TopCategories from '../components/modules/TopCategories'
+import ListPosts from '../components/posts/ListPosts'
+import PopularPosts from '../components/posts/PopularPosts'
 import { useFindLatestQuery } from '../generated/graphql'
 import withApollo from '../lib/withApollo'
 
@@ -14,7 +16,13 @@ const Index: React.FC<IndexProps> = ({}) => {
     <>
       <Navigation />
       <div className="mt-24 w-5/6 mx-auto flex items-start">
-        <IndexLayout />
+        <div className="w-full flex justify-between items-start">
+          <ListPosts className="md:w-4/6 w-full" />
+          <div className="hidden sticky ml-12 w-2/6 md:flex flex-col">
+            <TopCategories className="w-full" />
+            <PopularPosts className="mt-4" />
+          </div>
+        </div>
       </div>
     </>
   )
