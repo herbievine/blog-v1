a8a0a4ce8c8118c3564f94826689d894fe22cfce
Tim Vine <Home@tims-macbook-pro.home>
2021-06-04 12:01:56 +0200

    removed strict ts

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 const { i18n } = require('./next-i18next.config')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = {
+  webpack(config, options) {
+    const { dev, isServer } = options
+
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
+
+    return config
+  },
   future: {
     webpack5: true,
   },
