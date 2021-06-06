31e0fa7e08cef1ffc5864ea60418e618e2793bee
Tim Vine <Home@tims-macbook-pro.home>
2021-06-06 11:38:16 +0200

    fixed unused dependencies

--- lib/withApollo.ts
+++ lib/withApollo.ts
@@ -1,12 +1,8 @@
 import { withApollo } from 'next-apollo'
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
-import fetch from 'isomorphic-unfetch'
 
 export const apolloClient = new ApolloClient({
-  link: createHttpLink({
-    fetch,
-    uri: process.env.NEXT_PUBLIC_API_URL + '/graphql',
-  }),
+  uri: process.env.NEXT_PUBLIC_API_URL + '/graphql',
   cache: new InMemoryCache(),
 })
 
